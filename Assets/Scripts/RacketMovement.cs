bb74f6306d36572fcb13a2d2fdb20334089a4b81
Anshu_Nair <anair14@myseneca.ca>
2019-12-28 00:01:29 -0500

    AddedRacketMovement

---
+++ Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour
+{
+    public int moveSpeed = 5;
+
+    void Update()
+    {
+        if (transform.position.x > 0)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.Translate((new Vector3(0, moveSpeed, 0)) * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.Translate((new Vector3(0, -moveSpeed, 0)) * Time.deltaTime);
+        }
+
+        if (transform.position.x < 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+                transform.Translate((new Vector3(0, moveSpeed, 0)) * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.S))
+                transform.Translate((new Vector3(0, -moveSpeed, 0)) * Time.deltaTime);
+        }
+
+    }
+}
