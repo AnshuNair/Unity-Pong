0e578c4cf302a9e92981856654a602a352a40ccf
Anshu_Nair <anair14@myseneca.ca>
2019-12-28 01:36:32 -0500

    AddedInitialBallMovement

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public int ballSpeed;
+    Vector3 randomDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomDirection = new Vector3(Random.value, Random.value, 0.0f).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(randomDirection * ballSpeed * Time.deltaTime);
+    }
+}
