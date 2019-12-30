b18b3e607eb76af8490d7489bfd6af0774d07350
Anshu_Nair <anair14@myseneca.ca>
2019-12-30 02:04:00 -0500

    AddedBouncingLogic

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -10,7 +10,7 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        direction = new Vector3(Random.value, Random.value, 0.0f).normalized;
+        direction = new Vector3(Random.Range(-30, 45), Random.Range(-30, 45), 0.0f).normalized;
     }
 
     // Update is called once per frame
@@ -21,8 +21,35 @@ public class BallMovement : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        float x = direction.x;
         Vector3 orthogonalVector = collision.contacts[0].point - transform.position;
         float collisionAngle = Vector3.Angle(orthogonalVector, direction);
-        Debug.Log("Collision Angle: " + collisionAngle);
+        Debug.Log("OLD Direction: " + direction);
+        if (direction.y >= 0 && direction.x >= 0)
+        {
+            direction = new Vector3(Random.Range(30, 45) + x, Random.Range(-45, -30), 0.0f).normalized;
+            Debug.Log("+,+");
+        }
+
+        else if (direction.y >= 0 && direction.x <= 0)
+        {
+            direction = new Vector3(Random.Range(-45, -30) - x, Random.Range(-45, -30), 0.0f).normalized;
+            Debug.Log("-,+");
+        }
+
+        else if (direction.y <= 0 && direction.x >= 0)
+        {
+            direction = new Vector3(Random.Range(30, 45) + x, Random.Range(30, 45), 0.0f).normalized;
+            Debug.Log("+,-");
+        }
+
+        else
+        {
+            direction = new Vector3(Random.Range(-45, -30) - x, Random.Range(30, 45), 0.0f).normalized;
+            Debug.Log("-,-");
+        }
+
+        Debug.Log("NEW Direction: " + direction);
+        transform.Translate(direction * ballSpeed * Time.deltaTime);
     }
 }
