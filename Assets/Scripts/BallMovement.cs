93a2360291406d59df66aa0a10f1821566d99ccb
Anshu_Nair <anair14@myseneca.ca>
2019-12-31 00:45:03 -0500

    AddedGameOverMechanic

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,27 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
-    public float ballSpeed;
     public Vector3 direction;
-    public GameController gm;
+    GameController gm;
 
     // Start is called before the first frame update
     void Start()
     {
+        gm = GameObject.Find("GameController").GetComponent<GameController>();
         direction = new Vector3(Random.Range(-30, 45), Random.Range(-30, 45), 0.0f).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * ballSpeed * Time.deltaTime);
+        transform.Translate(direction * gm.ballSpeed * Time.deltaTime);
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        bool isGameOver = false;
+        gm = GameObject.Find("GameController").GetComponent<GameController>();
+
+        if (collision.gameObject.tag == "GoalWall")
+        {
+            if (collision.gameObject.transform.position.x > 0)
+            {
+                gm.scoreOne++;
+                gm.playerOneScore.text = gm.scoreOne.ToString();
+            }
+
+            else if (collision.gameObject.transform.position.x < 0)
+            {
+                gm.scoreTwo++;
+                gm.playerTwoScore.text = gm.scoreTwo.ToString();
+            }
+
+            if (gm.scoreOne >= 5 || gm.scoreTwo >= 5)
+            {
+                isGameOver = true;
+                Debug.Log("Game Over!");
+                gm.GameOver.SetActive(true);
+                Text goText = gm.GameOver.GetComponent<Text>();
+                goText.text = "Game Over!";
+                if (gm.scoreOne >= 5)
+                    goText.text += "Player One has Won. Press R to restart.";
+                else if (gm.scoreTwo >= 5)
+                    goText.text += "Player Two has Won. Press R to restart.";
+                //StartCoroutine(gm.restartGame(KeyCode.R));
+                Destroy(this.gameObject);
+            }
+
+            if (!isGameOver)
+            {
+                gm.SpawnBall();
+                Destroy(this.gameObject);
+            }
+
+        }
+
         float x = direction.x;
         float y = direction.y;
         Vector3 orthogonalVector = collision.contacts[0].point - transform.position;
@@ -58,14 +99,8 @@ public class BallMovement : MonoBehaviour
                 direction = new Vector3(Random.Range(15, 60), Random.Range(-60, -15), 0.0f).normalized;
         }
 
-        if (collision.gameObject.tag == "GoalWall")
-        {
-            gm.SpawnBall();
-            Destroy(this.gameObject);
-        }
-
         if (collision.gameObject.tag == "Racket")
-            ballSpeed += 0.5f;
-        transform.Translate(direction * ballSpeed * Time.deltaTime);
+            gm.ballSpeed += 0.5f;
+        transform.Translate(direction * gm.ballSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public Text playerOneScore;
+    public Text playerTwoScore;
+    public GameObject GameOver;
+    public int scoreOne;
+    public int scoreTwo;
+    public float ballSpeed;
     public GameObject ball;
+
+    void Awake()
+    {
+        SpawnBall();
+    }
+
+    void Start()
+    {
+        scoreOne = 0;
+        scoreTwo = 0;
+        playerOneScore.text = scoreOne.ToString();
+        playerTwoScore.text = scoreTwo.ToString();
+        ballSpeed = 3.0f;
+        Text goText = GameOver.GetComponent<Text>();
+        goText.text = "";
+        GameOver.SetActive(false);
+    }
 
     public void SpawnBall()
     {
         GameObject newBall = Instantiate(ball, new Vector3(0.0f, 0.0f, 1.0f), Quaternion.identity);
         BallMovement bm = newBall.GetComponent<BallMovement>();
         bm.direction = new Vector3(Random.Range(-30, 45), Random.Range(-30, 45), 0.0f).normalized;
-        bm.ballSpeed = 3;
-        bm.transform.Translate(bm.direction * bm.ballSpeed * Time.deltaTime);
+        bm.transform.Translate(bm.direction * ballSpeed * Time.deltaTime);
     }
+
+    public IEnumerator restartGame(KeyCode keyCode)
+    {
+        while (!Input.GetKeyDown(keyCode))
+        {
+            Debug.Log("Inside the restart coroutine");
+            yield return null;
+        }
+
+        Debug.Log("User Pressed R!");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
